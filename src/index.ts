6cc3d9137ee7bec32323e011892e72e82fc09fc2
Kobi <qobyyy@gmail.com>
2019-10-22 14:14:20 +0300

    chore: add async iterator syntax (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-import * as minimist from "minimist"
-import { Option } from "tsla-util/lib/option"
 import { exec as nodeExec } from "child_process"
-import { promisify } from "util"
-import { join, basename, dirname } from "path"
-import { extract } from "tar-stream"
-import * as request from "request"
 import { createWriteStream, promises as fs } from "fs"
+import * as minimist from "minimist"
+import * as pEvent from "p-event"
+import { basename, dirname, join } from "path"
+import * as request from "request"
+import { extract } from "tar-stream"
+import { Option } from "tsla-util/lib/option"
+import { promisify } from "util"
 import { createGunzip } from "zlib"
-import { PassThrough } from "stream"
 import { maybePackageJSON } from "./maybePackageJSON"
 
 const exec = promisify(nodeExec)
@@ -17,18 +17,6 @@ export interface Args {
   package?: string
   srcPattern?: string
 }
-
-export interface CompiledFile {
-  pathArray: string[]
-  path: string
-  stream: PassThrough
-}
-
-export type Node<T> =
-  | T
-  | ({
-      [key: string]: Node<T>
-    })
 
 async function main() {
   const ignore = ["package.json", "README.md", "CHANGELOG.md"]
@@ -46,11 +34,16 @@ async function main() {
       `npm view ${packageName} dist.tarball`
     )
 
-    const response = request(tarUrl)
+    const tarStream = request(tarUrl)
+      .pipe(createGunzip())
+      .pipe(extract())
 
-    const tarStream = response.pipe(createGunzip()).pipe(extract())
+    const asyncIterator = pEvent.iterator(tarStream, "entry", {
+      multiArgs: true,
+      resolutionEvents: ["finish"]
+    })
 
-    tarStream.on("entry", async (header, stream, next) => {
+    for await (const [header, stream, next] of asyncIterator) {
       const { name } = header
       const pathArray = name.split("/").slice(1)
 
@@ -59,26 +52,21 @@ async function main() {
       const dir = dirname(filePath)
 
       if (ignore.indexOf(jsBasename) === -1) {
-        // setIn(dirTree, dir.split(sep), dir)
         await fs.mkdir(dir, { recursive: true })
         stream.pipe(createWriteStream(filePath))
       } else {
         stream.resume()
       }
 
-      stream.on("end", () => {
-        next()
-      })
-    })
+      await pEvent(stream, "end")
+      next()
+    }
 
-    tarStream.on("finish", async () => {
-      // touch a source file to make them newer than extracted files
-      const { srcPattern = "src/index.ts" } = args
-      // const srcFiles = await glob(srcPattern)
-      const srcFiles = [srcPattern]
-      const now = new Date()
-      await Promise.all(srcFiles.map(srcFile => fs.utimes(srcFile, now, now)))
-    })
+    // touch a source file to make them newer than extracted files
+    const { srcPattern = "src/index.ts" } = args
+    const srcFiles = [srcPattern]
+    const now = new Date()
+    await Promise.all(srcFiles.map(srcFile => fs.utimes(srcFile, now, now)))
   } catch (e) {
     console.error(e)
   }
